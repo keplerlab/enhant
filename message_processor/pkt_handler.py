45397ad04dbf98ba0b85bee0d41b4762f519b7f0
Mayank Jain <mayank10.j@gmail.com>
2020-08-17 15:17:19 +0530

    updated checker code

--- message_processor/pkt_handler.py
+++ message_processor/pkt_handler.py
@@ -6,10 +6,55 @@ def extract_data_from_msg(pkt):
 
 
 def isRequestValid(pkt):
-    if ("msg" in pkt) and ("context" in pkt):
-        return True
-    else:
+    if ("msg" not in pkt):
+        print("msg field missing", flush=True)
         return False
+
+    if ("context" not in pkt):
+        print("context field missing", flush=True)
+        return False
+
+    if ("origin" not in pkt["context"]):
+        print("origin field missing", flush=True)
+        return False
+
+    if ("meeting_id" not in pkt["context"]):
+        print("meeting_id field missing", flush=True)
+        return False
+
+    if ("event_time" not in pkt["context"]):
+        print("event_time field missing", flush=True)
+        return False
+
+    if ("name" not in pkt["msg"]):
+        print("msg.name field missing", flush=True)
+        return False
+
+    if ("type" not in pkt["msg"]):
+        print("msg.type field missing", flush=True)
+        return False
+
+    if ("desc" not in pkt["msg"]):
+        print("desc field missing", flush=True)
+        return False
+
+    if ("data" not in pkt["msg"]):
+        print("msg.data field missing", flush=True)
+        return False
+
+    if ("version" not in pkt["msg"]):
+        print("msg.version field missing", flush=True)
+        return False
+
+    if (pkt["context"]["origin"] != "host" and pkt["context"]["origin"] != "guest"):
+        print("context.origin is neither guest or host", flush=True)
+        return False
+
+    if (pkt["msg"]["name"] != "INIT" and pkt["msg"]["name"] != "END" and pkt["msg"]["name"] != "ADD" and pkt["msg"]["name"] != "DELETE"):
+        print("msg.name is not among values: INIT, ADD, END, DELETE", flush=True)
+        return False
+
+    return True
 
 
 def prepare_response(pkt, is_ok, inserted_record_id):
