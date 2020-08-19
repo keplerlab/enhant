19a7dff338175c07242f5d7577a491d9b2673627
Mayank Jain <mayank10.j@gmail.com>
2020-08-19 17:33:13 +0530

    init messge_processor logic bug fix

--- message_processor/conversation_messages.py
+++ message_processor/conversation_messages.py
@@ -5,13 +5,17 @@
 """
 
 class Conversation(object):
-    """Client for handling notes"""
+    """Client for handling conversations"""
 
     def __init__(self, mongo_client, pkt):
         self.conversation_collection = "conversations"
         self.processed_conversation_collection = "conversations_processed"
         self.pkt = pkt
         self.mongo_client = mongo_client
+        # Time window for checking whether two conversations with same meeting number 
+        # are part of same conversation or not in ms 
+        #self.time_window = 120*60*1000 
+        self.time_window = 1000
 
     
     def _process_init_json(self):
@@ -21,36 +25,78 @@ class Conversation(object):
                       }
         return result_json
 
-       
+
     async def _insert_new_conv(self):
         result_json = self._process_init_json()
-        print("result_json",result_json,flush=True)
         result = await self.mongo_client.insert_json(result_json, self.processed_conversation_collection)
         inserted_conv_id = str(result.inserted_id)
-        print("_id",result)
-        print('inserted_id for record', inserted_conv_id, flush=True)
         self.pkt["context"]["conv_id"] = inserted_conv_id
-        print("self.pkt", self.pkt, flush=True)
         result = await self.mongo_client.insert_json(self.pkt, self.conversation_collection)
 
         return inserted_conv_id
 
+    async def _insert_old_conv(self, conv_id):
+        inserted_conv_id = str(conv_id)
+        self.pkt["context"]["conv_id"] = inserted_conv_id
+        result = await self.mongo_client.insert_json(self.pkt, self.conversation_collection)
+
+        return inserted_conv_id
+
+    def _check_record_in_time_window(self, list_db_processed_conv):
+        print("inside _check_record_in_time_window list_db_processed_conv:", list_db_processed_conv, flush=True)
+        print("Len list_db_processed_conv:", len(list_db_processed_conv), flush=True)
+        packet_time = int(self.pkt["context"]["event_time"])
+
+        nearest_conv_time = 0
+        nearest_conv = None
+        for processed_conv in list_db_processed_conv:
+            print("result_conv", processed_conv, flush=True)
+            if "end_time" in processed_conv:
+                processed_conv_time = int(processed_conv["end_time"])
+            else:
+                processed_conv_time = int(processed_conv["start_time"])
+    
+            if (packet_time - nearest_conv_time) > (packet_time - processed_conv_time):
+                nearest_conv_time = processed_conv_time
+                nearest_conv = processed_conv
+            
+        #print("nearest_conv_time", nearest_conv_time, flush=True)
+        #print("processed_conv_time", processed_conv_time, flush=True)
+
+
         
+        if (packet_time - nearest_conv_time) <= self.time_window:
+            print("Nearest conv", nearest_conv, flush=True)
+            print("Nearest conv time", nearest_conv_time, flush=True)
+            return nearest_conv
+        else:
+            print("No nearby conv found", flush=True)
+            return None
+
+
     async def save_conversation(self):
         if self.pkt["msg"]["name"] == "INIT":
             meeting_id = self.pkt["context"]["meeting_id"]
 
             query = {"meeting_id": str(meeting_id)}
 
-            list_conversation_documents = await self.mongo_client.findQueryProcessor(query, self.processed_conversation_collection)
-            print("conversation_document", list_conversation_documents,  flush=True)
+            list_db_processed_conv = await self.mongo_client.findQueryProcessor(query, self.processed_conversation_collection)
+            print("conversation_document", list_db_processed_conv,  flush=True)
             # Add new record in conversation if no result found  
-            if len(list_conversation_documents) <= 0:
+            if len(list_db_processed_conv) <= 0:
                 print(f"No matching conversation for meeting ID: {meeting_id} inserting new record",  flush=True)
                 inserted_conv_id = await self._insert_new_conv()
                 return inserted_conv_id
             else:
-                pass
+                nearest_record = self._check_record_in_time_window(list_db_processed_conv)
+                if nearest_record is None:
+                    inserted_conv_id = await self._insert_new_conv()
+                    return inserted_conv_id
+                else:
+                    conv_id = nearest_record.get('_id')
+                    inserted_conv_id = await self._insert_old_conv(conv_id)
+                    return inserted_conv_id
+
             
         if self.pkt["msg"]["name"] == "END":
             conv_id = self.pkt["context"]["conv_id"]
