4b0136bc29f33bc9aee7262f88b9ce6e012ede2f
Mayank Jain <mayank10.j@gmail.com>
2020-08-26 13:27:31 +0530

    srt parsing

--- cli/srt_helper.py
+++ cli/srt_helper.py
@@ -7,8 +7,22 @@ import sys
 import os
 import pysrt
 
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def _transformTime(srt_time, start_utc_time=0):
+    """[summary]
+
+    :return: [description]
+    :rtype: [type]
+    """
+    minutes = srt_time.minutes
+    seconds = srt_time.seconds + 60*minutes
+    milliseconds = srt_time.milliseconds + seconds*1000 + start_utc_time
+    return milliseconds
+
 
 def transformSrt(srtList, meeting_start_utc, meeting_end_utc):
     """[transform srt packet]
@@ -26,8 +40,8 @@ def transformSrt(srtList, meeting_start_utc, meeting_end_utc):
         transcriptions_pkt['msg']['data']['transcription'] = {}
         #transcriptions_pkt['msg']['data']['transcription']['content'] = {}
         transcriptions_pkt["msg"]["data"]["transcription"]["content"] = srt.text
-        transcriptions_pkt["msg"]["data"]["transcription"]["start_time"] = srt.start.milliseconds
-        transcriptions_pkt["msg"]["data"]["transcription"]["end_time"] = srt.end.milliseconds
+        transcriptions_pkt["msg"]["data"]["transcription"]["start_time"] = _transformTime(srt.start, meeting_start_utc)
+        transcriptions_pkt["msg"]["data"]["transcription"]["end_time"] = _transformTime(srt.end, meeting_start_utc)
         print("transcriptions_pkt", transcriptions_pkt)
         transcription_pkt_list.append(transcriptions_pkt)
 
