99da55957e2853fafaabf5a66641b6be006a5dc7
Mayank Jain <mayank10.j@gmail.com>
2020-09-08 13:37:34 +0530

    updated dockerfile

--- api/transcription_svc/Docker-kaldi/build.sh
+++ api/transcription_svc/Docker-kaldi/build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-docker build --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag keplerlab/kaldi-vosk-wesocket-server:0.1.1 .
+docker build --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag keplerlab/kaldi-vosk-wesocket-server:0.1.2 .
 
-docker push keplerlab/kaldi-vosk-wesocket-server:0.1.1
+docker push keplerlab/kaldi-vosk-wesocket-server:0.1.2
 
