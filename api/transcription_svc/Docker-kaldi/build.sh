db780bf2d01524b9408e747f203bc0af31a40b7b
Mayank Jain <mayank10.j@gmail.com>
2020-09-08 11:42:58 +0530

    update dockerfiles

--- api/transcription_svc/Docker-kaldi/build.sh
+++ api/transcription_svc/Docker-kaldi/build.sh
@@ -3,8 +3,7 @@
 set -e
 set -x
 
-docker build --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag keplerlab/kaldi-vosk-wesocket-server:latest .
+docker build --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag keplerlab/kaldi-vosk-wesocket-server:0.1.1 .
 
-
-docker push keplerlab/kaldi-vosk-wesocket-server:latest
+docker push keplerlab/kaldi-vosk-wesocket-server:0.1.1
 
--- cli/Docker-cli/build.sh
+++ cli/Docker-cli/build.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-docker build --file Dockerfile --tag keplerlab/enhant-cli:latest .
-docker push keplerlab/enhant-cli:latest
+docker build --file Dockerfile --tag keplerlab/enhant-cli:0.1.1 .
+docker push keplerlab/enhant-cli:0.1.1
 
