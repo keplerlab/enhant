8bb7533fd5a4fb0d56feddbf29c4025e39c7670b
Nitin Katyal <nitin.katyal@publicissapient.com>
2020-09-11 09:21:32 +0530

    change notification messages and default text to enable plugin, command to take note added as well

--- plugins/chrome/background.js
+++ plugins/chrome/background.js
@@ -321,14 +321,32 @@ chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.storage.local.set(obj, function(){
         console.log("Object saved into local storage: ", obj);
 
-        // add to storage that plugin is activated
-        chrome.tabs.sendMessage(tab.id, {cmd: "activate_plugin"}, function(result) {
-            console.log("Plugin activated : ", result.status);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // add to storage that plugin is activated
+            chrome.tabs.sendMessage(tabs[0].id, {cmd: "activate_plugin"}, function(result) {
+                console.log("Plugin activated : ", result.status);
+            });
+    
         });
-
+        
     });
 
     
   });
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "take-note") {
+
+        // add to storage that plugin is activated
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+            // add to storage that plugin is activated
+            chrome.tabs.sendMessage(tabs[0].id, {cmd: "take-note"}, function(result) {
+                console.log("Plugin activated : ", result.status);
+            });
+    
+        });
+    }
+});
 
 console.log("Backround Script Loaded from extension - [enhan(t)]");
