14ae383a948ea4a4a2df264622625cbbf6109d34
Nitin Katyal <nitin.katyal@publicissapient.com>
2020-09-25 13:50:18 +0530

    notify when window is loaded so scripts doesnt re-run in redirections

--- plugins/chrome/notifyPageloaded.js
+++ plugins/chrome/notifyPageloaded.js
@@ -1,4 +1,6 @@
 //chrome script to notify page load  - This will eventually load enhant content
-chrome.runtime.sendMessage({msg: "page-loaded"}, function(response){
-    console.log(" Status for notify upload ", response);
-});
+window.onload = function(e){
+    chrome.runtime.sendMessage({msg: "page-loaded"}, function(response){
+        console.log(" Status for notify upload ", response);
+    });
+}
