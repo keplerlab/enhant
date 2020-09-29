2c03db97c5f9ee052aeb259ff93fb97ce125f78f
Mayank Jain <mayank10.j@gmail.com>
2020-09-29 17:16:00 +0530

    added cli in batch mode

---
+++ cli/enhant-cli-batch.py
@@ -0,0 +1,74 @@
+from google.cloud import storage
+
+import typer
+from typing import List
+import os
+from typing import NoReturn, Tuple
+
+
+from operator import methodcaller
+import json
+import zipfile
+import shutil
+
+from colorama import init, Fore, Back
+
+init(init(autoreset=True))
+
+APP_NAME = "enhant-cli-batch"
+
+app = typer.Typer()
+
+def create_bucket_class_location(bucket_name):
+    """Create a new bucket in specific location with storage class"""
+    bucket_name = "enhant-testing"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    bucket.storage_class = "STANDARD"
+    new_bucket = storage_client.create_bucket(bucket, location="us")
+
+    print(
+        "Created bucket {} in {} with storage class {}".format(
+            new_bucket.name, new_bucket.location, new_bucket.storage_class
+        )
+    )
+    return new_bucket
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "enhant-testing"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+#create_bucket_class_location("enhant-testing")
+
+@app.command()
+def test_batch_wav(input: str) -> NoReturn:
+    print("input", input)
+    if input.endswith(".wav"):
+        print(Fore.GREEN + f"\n Uploading data", input)
+
+        upload_blob("enhant-testing", input, "LLP_test.wav")
+    else:
+        print(Fore.RED + f"\n ERROR: Invalid zip file or folder")
+        return 0
+
+#upload_blob("enhant-testing", "./LLP.wav", "LLP.wav")
+
+if __name__ == "__main__":
+    app()
---
+++ cli/speaker_dirazation_long_audio.js
@@ -0,0 +1,69 @@
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech').v1p1beta1;
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const gcsUri = 'gs://enhant-testing/Agent.wav';
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-IN';
+const speakerDiarizationConfig = 
+{
+    "enableSpeakerDiarization": true,
+    "minSpeakerCount": 1,
+    "maxSpeakerCount": 5
+    //"speakerTag": integer
+}
+
+const config = {
+
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  enableSpeakerDiarization: true,
+  enableAutomaticPunctuation: true,
+  languageCode: languageCode,
+  SpeakerDiarizationConfig:speakerDiarizationConfig,
+  //model: 'phone_call',
+  //diarizationSpeakerCount: 2,
+
+};
+
+const audio = {
+  uri: gcsUri,
+};
+
+const request = {
+  config: config,
+  audio: audio,
+};
+
+
+
+(async () => { 
+    const [operation] = await client.longRunningRecognize(request);
+    // Get a Promise representation of the final result of the job
+    const [response] = await operation.promise();
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+    //console.log(`response.results: ${response.results}`);
+
+    console.log('Speaker Diarization:');
+    const result = response.results[response.results.length - 1];
+    //console.log(`result: ${JSON.stringify(result)}`);
+    const wordsInfo = result.alternatives[0].words;
+    console.log(`wordsInfo: ${JSON.stringify(wordsInfo)}`);
+
+    // Note: The transcript within each result is separate and sequential per result.
+    // However, the words list within an alternative includes all the words
+    // from all the results thus far. Thus, to get all the words with speaker
+    // tags, you only have to take the words list from the last result:
+    //wordsInfo.forEach(a =>
+    //console.log(` word: ${a.word}, speakerTag: ${a.speakerTag}`)
+    //);
+ })()
